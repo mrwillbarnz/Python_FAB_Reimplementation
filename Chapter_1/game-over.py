cf6465daaf644bde45733b2aed84efd1473d5e28
Will Barnard <willbarnard687@pm.me>
2020-02-08 21:15:02 +0000

    Initial commit

---
+++ Chapter_1/game-over.py
@@ -0,0 +1,8 @@
+# Game-Over Script
+print("My name's Bim!")
+
+print("Speeeeed!")
+print("Jeremy Clarkson")
+
+input("\n\n Press the Enter key to exit.")
+
---
+++ Chapter_1/hello-world.py
@@ -0,0 +1,6 @@
+# Hello-World Test!
+print("Hello World!")
+
+input("\n\nPress the enter key to exit.")
+
+
