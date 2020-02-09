6d35a9d49e4f6b775236b501b34df190dbec84df
Will Barnard <willbarnard687@pm.me>
2020-02-09 20:01:50 +0000

    Added Chapter 2: Word_Problems Script! Made sure to close all statements and individualise each print statement, wanted to make a nested statement, might do this is a future revision.

---
+++ Chapter_2/word_problems.py
@@ -0,0 +1,53 @@
+# Word Problems
+# Demonstrates numbers and maths
+
+print (\
+"""
+If a pregnant hippo, weighing 2,000 pounds, gives birth to a 100-pound calf,
+but then eats 50 pounds of food, how much does she weigh?
+""")
+input("Press the enter key to find out.")
+print ("2000 - 100 + 50 = "),
+print (2000 - 100 + 50)
+
+print (\
+"""
+If an adventurer returns from a successful quest and buys each of
+6 companions 3 bottles of ale, how many bottles does the adventurer buy?
+""")
+input("Press the enter key to find out.")
+print("6 * 3 = "),
+print (6 * 3)
+
+print (\
+"""
+If a kid has 24 pieces of halloween candy and eats 6 pieces a day,
+how many days will the stash last?
+""")
+input("Press the enter key to find out.")
+print("24 / 6 = "),
+print(24 / 6)
+
+print (\
+"""
+If a group of four pirates finds a chest full of 107 gold coins,
+and they divide the booty evenly, how many coins will be left over?
+""")
+input("Press the enter key to find out.")
+print("107 % 4 = "),
+print(107 % 4)
+
+print (
+"""
+If a restaurant check comes to 19 dollars with tip, and you and your friends
+split it evenly 4 ways, how much do you each throw in?
+""")
+input("Press the enter key to find out.")
+print ("19 / 4 = ")
+print (19 / 4)
+print ("WRONG!")
+input("Press the enter key for the right answer.")
+print (19.0 / 4)
+
+input("Press the enter key to exit.")
+
