fa48e9b49d61aff71c0f962715d08de07dcb8821
Will Barnard <willbarnard687@pm.me>
2020-02-09 19:34:31 +0000

    Added Chapter 2 Script 'Silly Strings', modified .vscode file

--- Chapter_2/silly_strings.py
+++ Chapter_2/silly_strings.py
@@ -5,5 +5,17 @@ print("You can concatenate two " + "strings with the '+' operator.")
 
 print("\n This string " + "may not " + "seem terr" + "ibly impressive. " \
     + "But what " + "you don't know " + " long string, created from concatenation " \
-    + "of " + "thirty-two " + " Now are you" + "impressed?\n\n" + "See, " \
-    + " it look " + " as " + "if " +  )
+    + "of " + "thirty-two " + " Now are you " + "impressed?\n\n" + "See, " \
+    + " it looks " + " as " + "if " + "it " + "'s " + "got " + "to "
+    + "be" + " multiple strings." + " Okay, now this " + "one " \
+    + "long" + " string is over!" )
+
+print (
+"""
+If you really like a string, you can repeat it. For example, who doesn't like pie?
+That's right, nobody. But if you really like it, you should say it like you meant it:
+""",
+
+print("\n\n Now that's good eating."))
+
+input("\n\n Press the enter key to exit.")
