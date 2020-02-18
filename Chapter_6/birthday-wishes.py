2dcf3c3ad98cd029725bcb31b059d42db141c945
Will Barnard <willbarnard687@pm.me>
2020-02-18 18:45:30 +0000

    Chapter_6 commit #2
    birthday-wishes and global-reach enhancements, added birthday functions and global variable examples in global-reach.py

--- Chapter_6/birthday-wishes.py
+++ Chapter_6/birthday-wishes.py
@@ -6,3 +6,19 @@ def birthday1(name, age):
     print("Happy birthday,", name, "!", "I hear you're", age, "today.\n")
 
 # parameters with default values
+def birthday2(name = "Jackson", age = 1):
+    print("Happy birthday,", name, "!", " I hear you're", age, "today.\n")
+
+# birthday1() function
+birthday1("Jackson", 1)
+birthday1(1, "Jackson")
+birthday1(name = "Jackson", age = 1)
+birthday1(age = 1, name = "Jackson")
+# birthday2() function
+birthday2()
+birthday2(name = "Katherine")
+birthday2(age = 12)
+birthday2(name = "Katherine", age = 12)
+birthday2("Katherine", 12)
+
+input("\n\nPress the enter key to exit.")
