4c3614f137f372fdca6056cdc2e46a206a9bc08b
Will Barnard <willbarnard687@pm.me>
2020-02-26 12:44:21 +0000

    Chapter_9 and 10 Commit
    Added Chapters 9 and 10 to the repo, used tkinter for now, gtk versions will be in the sub-folder 'gtk' in Chapter 10 (yet to be committed).

---
+++ Chapter_10/simple-gui.py
@@ -0,0 +1,15 @@
+# Simple GUI
+# Demonstrates creating a windows
+
+from tkinter import *
+
+# create the root windows
+root = Tk()
+
+# modify the window
+root.title("Simple GUI")
+root.geometry("200x100")
+
+# Kick off the main window's event loop
+root.mainloop()
+
